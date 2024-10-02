761966e9516863a6505a78eb27177855e5bd30c6
Artem Vakhitov <artem.vakhitov@gmail.com>
2024-10-02 18:27:35 +0300

    Add remote to build and create deploy

--- config.tf
+++ config.tf
@@ -13,7 +13,6 @@ provider "yandex" {
 
 resource "yandex_compute_instance" "build" {
 
-
   name = "build"
 
   zone = "ru-central1-b"
@@ -37,6 +36,58 @@ resource "yandex_compute_instance" "build" {
 
   metadata = {
     user-data = "${file("build_meta.txt")}"
+  }
+
+  connection {
+    host = self.network_interface.0.nat_ip_address
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/devops-eng-yandex-kp.pem")
+    timeout = "3m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /tmp && git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git",
+      "cd /tmp && mvn package"
+    ]
+  }
+
+}
+
+resource "yandex_compute_instance" "deploy" {
+
+  name = "deploy"
+
+  zone = "ru-central1-b"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8tvc3529h2cpjvpkr5"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e2lgv5mqm56n8fjkt37q"
+    nat = true
+  }
+
+  metadata = {
+    user-data = "${file("deploy_meta.txt")}"
+  }
+
+  connection {
+    host = self.network_interface.0.nat_ip_address
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/devops-eng-yandex-kp.pem")
+    timeout = "3m"
   }
 
 }
