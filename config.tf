51804387a822fb1225245ef811de722fc8863db8
Artem Vakhitov <artem.vakhitov@gmail.com>
2024-10-02 17:05:51 +0300

    Initial commit with cloud config

---
+++ config.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-b"
+}
+
+resource "yandex_compute_instance" "build" {
+
+
+  name = "build"
+
+  zone = "ru-central1-b"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8tvc3529h2cpjvpkr5"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e2lgv5mqm56n8fjkt37q"
+    nat = true
+  }
+
+  metadata = {
+    user-data = "${file("build_meta.txt")}"
+  }
+
+}
