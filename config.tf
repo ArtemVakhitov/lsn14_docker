2785be33c697abcd5d8077e0e325ea7ed48c12db
Artem Vakhitov <artem.vakhitov@gmail.com>
2024-10-03 21:32:46 +0300

    Do use local_sensitive_file after all

--- config.tf
+++ config.tf
@@ -85,7 +85,7 @@ resource "yandex_compute_instance" "build" {
       host = self.network_interface.0.nat_ip_address
       type = "ssh"
       user = "ubuntu"
-      private_key = "${data.local_file.build_private_key.content}"
+      private_key = "${data.local_sensitive_file.build_private_key.content}"
     }
 
   }
@@ -128,7 +128,7 @@ resource "yandex_compute_instance" "deploy" {
       host = self.network_interface.0.nat_ip_address
       type = "ssh"
       user = "ubuntu"
-      private_key = "${data.local_file.deploy_private_key.content}"
+      private_key = "${data.local_sensitive_file.deploy_private_key.content}"
     }
 
   }
