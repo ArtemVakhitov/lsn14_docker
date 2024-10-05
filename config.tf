d8fab0c67661aba67bbd5521cb12f5c0a20eeb61
Artem Vakhitov <artem.vakhitov@gmail.com>
2024-10-05 18:19:06 +0300

    Comment out docker provider

--- config.tf
+++ config.tf
@@ -3,10 +3,10 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
+    # docker = {
+    #   source  = "kreuzwerker/docker"
+    #   version = "3.0.2"
+    # }
   }
   required_version = ">= 0.13"
 }
